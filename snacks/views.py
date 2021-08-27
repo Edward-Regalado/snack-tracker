a8e6ad0b4ae77c22bb7480fa239cb9afb5ad8642
Edward-Regalado <tregalado30@gmail.com>
2021-08-26 23:07:24 -0700

    added models, views, templates and tests

--- snacks/views.py
+++ snacks/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.db import models
 from django.views.generic import ListView, DetailView
 from .models import Snacks
 
 # Create your views here.
 
-class SnackListView(ListView):
+class SnackListViews(ListView):
     template_name = 'snack_list.html'
     model = Snacks
-    context_object_name = 'sw_snacks'
+    # context_object_name = 'sw_snacks'
     
-class SnackDetailView(DetailView):
+class SnackDetailViews(DetailView):
     template_name = 'snack_detail.html'
     model = Snacks
